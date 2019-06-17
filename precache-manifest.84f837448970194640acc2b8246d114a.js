d84ee3d82dc1d2c69cfdaeead5f904d08e516441
dgbarr2 <dgbarr@mac.com>
2019-06-17 16:27:36 +0100

    Add files via upload

---
+++ precache-manifest.84f837448970194640acc2b8246d114a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12142c708cb685bb8a125490de930c35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd51ebe7b607db092023",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "bd51ebe7b607db092023",
+    "url": "/static/js/main.4ff2104d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
